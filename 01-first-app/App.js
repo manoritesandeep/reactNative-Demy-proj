a0ba80288cf825be2064a75bb17193c654b9d13c
manoritesandeep <manoritesandeep@gmail.com>
2025-04-06 00:31:37 -0700

    flatlist... instead of scrollview

--- 01-first-app/App.js
+++ 01-first-app/App.js
@@ -6,6 +6,7 @@ import {
   View,
   TextInput,
   ScrollView,
+  FlatList,
 } from "react-native";
 
 export default function App() {
@@ -21,7 +22,7 @@ export default function App() {
     // console.log("Add Goal Handler, enteredGoalText: ", enteredGoalText);
     setCourseGoals((currentCourseGoal) => [
       ...currentCourseGoal,
-      enteredGoalText,
+      { text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
 
@@ -36,13 +37,17 @@ export default function App() {
         <Button title="Add goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
     </View>
   );

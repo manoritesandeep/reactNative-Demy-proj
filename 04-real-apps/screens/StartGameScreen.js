ebfb6c3790abd5051bf754859a0e93efdf869981
manoritesandeep <manoritesandeep@gmail.com>
2025-04-07 09:23:25 -0700

    04-real-apps initial commit

---
+++ 04-real-apps/screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return;
+  <View>
+    <TextInput />
+    BUTTONS GO HERE!
+  </View>;
+}
+
+export default StartGameScreen;

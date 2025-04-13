c07a1f6b5b8e7ffbc6af1d48a1ff8445cc07e281
manoritesandeep <manoritesandeep@gmail.com>
2025-04-12 22:25:46 -0700

    Updated StartGameScreen.js with autoCapitalize to none and autoCorrect to false. also added default keyboard to number pad and limited character size to two...

--- 04-real-apps/screens/StartGameScreen.js
+++ 04-real-apps/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +32,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 75,
+    width: 75,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });

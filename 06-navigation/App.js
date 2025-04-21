1557551880d105c8c72950add9028dc9e60ad006
manoritesandeep <manoritesandeep@gmail.com>
2025-04-21 02:31:58 -0700

    Getting started with App and outputting meal categories... displaying items in a grid... next integrating React Navigation...

--- 06-navigation/App.js
+++ 06-navigation/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SystemUI from "expo-system-ui"; // ios background color hack // // npx expo install expo-system-ui
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //   flex: 1,
+    //   backgroundColor: "#24180f", // Set the default background color here
   },
 });
+
+SystemUI.setBackgroundColorAsync("#24180f"); // ios background color hack

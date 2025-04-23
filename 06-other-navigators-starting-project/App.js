2a5180e3c73c58b6bf73e11296e27169541b9f98
manoritesandeep <manoritesandeep@gmail.com>
2025-04-22 20:37:12 -0700

    111. Adding, Configuring & Using Bottom Tabs

--- 06-other-navigators-starting-project/App.js
+++ 06-other-navigators-starting-project/App.js
@@ -1,43 +1,46 @@
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+// import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 import { Ionicons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
+      {/* <Drawer.Navigator */}
+      <BottomTab.Navigator
         initialRouteName="User"
         screenOptions={{
           headerStyle: { backgroundColor: "#3c0a6b" },
           headerTintColor: "white",
-          drawerActiveBackgroundColor: "#f0e1ff",
-          drawerActiveTintColor: "#3c0a6b",
-          // drawerStyle: { backgroundColor: "#ccc" },
+          tabBarActiveTintColor: "#3c0a6b",
         }}
       >
-        <Drawer.Screen
+        {/* <Drawer.Screen */}
+        <BottomTab.Screen
           name="Welcome"
           component={WelcomeScreen}
           options={{
-            drawerLabel: "Welcome Screen",
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name="home" color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name="User"
           component={UserScreen}
           options={{
-            drawerIcon: <Ionicons name="person" color={color} size={size} />,
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="person" color={color} size={size} />
+            ),
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }

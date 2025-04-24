677268a7b82f575006942bd8f38d367874760b2a
manoritesandeep <manoritesandeep@gmail.com>
2025-04-23 23:28:59 -0700

    updated files for 07 redux version... initiial commit... need to remove context details as its a copy of 07 context version...

---
+++ 07-state-management-redux/components/MealsList/MealsList.js
@@ -0,0 +1,42 @@
+import { FlatList, View, StyleSheet } from "react-native";
+import MealItem from "./MealItem";
+
+function MealsList({ items }) {
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      id: item.id,
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+
+    return (
+      <MealItem {...mealItemProps} />
+      //   <MealItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* <Text>Meals Overview Screen - {catId}</Text> */}
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsList;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
--- 07-state-management-redux/store/context/favorites-context.js
+++ 07-state-management-redux/store/context/favorites-context.js
@@ -1,7 +1,35 @@
-import { Text } from "react-native";
+import { createContext, useState } from "react";
 
-function favoritesContext() {
-  return <Text>Context </Text>;
+export const FavoriteContext = createContext({
+  ids: [],
+  addFavorite: (id) => {},
+  removeFavorite: (id) => {},
+});
+
+function FavoriteContextProvider({ children }) {
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
+  return (
+    <FavoriteContext.Provider value={value}>
+      {children}
+    </FavoriteContext.Provider>
+  );
 }
 
-export default favoritesContext;
+export default FavoriteContextProvider;

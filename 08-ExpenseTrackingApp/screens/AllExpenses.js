92d4f545a63e4dc6466b3331a2d30568ce194660
manoritesandeep <manoritesandeep@gmail.com>
2025-04-24 22:08:58 -0700

    adding navigation and configuring... adding global colors and editing navigation config... creating key app components to display expenses... continue work on the expense related components... adding dummy expense data

---
+++ 08-ExpenseTrackingApp/screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+
+function AllExpenses() {
+  return <ExpensesOutput expensesPeriod="Total" />;
+}
+
+export default AllExpenses;
---
+++ 08-ExpenseTrackingApp/screens/RecentExpenses.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+import ExpensesOutput from "../components/Expenses/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOutput expensesPeriod="Last 7 Days" />;
+}
+
+export default RecentExpenses;

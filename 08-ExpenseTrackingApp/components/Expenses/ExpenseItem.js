0b7b41e814592d6ffa7c297cebd49096fd5b49ca
manoritesandeep <manoritesandeep@gmail.com>
2025-04-25 04:28:01 -0700

    added date formatting function in new util folder...

--- 08-ExpenseTrackingApp/components/Expenses/ExpenseItem.js
+++ 08-ExpenseTrackingApp/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { Text, View, Pressable, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/Styles";
+import { getFormattedDate } from "../../util/date";
 
 function ExpenseItem({ description, amount, date }) {
   return (
@@ -9,7 +10,7 @@ function ExpenseItem({ description, amount, date }) {
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
           <Text style={styles.amount}>{amount}</Text>
@@ -50,6 +51,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,

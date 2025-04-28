4bb48066431b8e767a52ae9d2f355435347aac51
manoritesandeep <manoritesandeep@gmail.com>
2025-04-28 09:37:05 -0700

    Adding validation... providing visual validation feedback... adding error styling... final push for section 9...

--- 09-HandlingUserInput-ExpenseTrackingApp/components/ManageExpense/ExpenseForm.js
+++ 09-HandlingUserInput-ExpenseTrackingApp/components/ManageExpense/ExpenseForm.js
@@ -1,35 +1,74 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 import { useState } from "react";
 import Button from "../UI/Button";
 import { getFormattedDate } from "../../util/date";
+import { GlobalStyles } from "../../constants/Styles";
 
 function ExpenseForm({ onCancel, submitButtonLabel, onSubmit, defaultValues }) {
-  const [inputValues, setInputValues] = useState({
-    amount: defaultValues ? defaultValues.amount.toString() : "",
-    date: defaultValues ? getFormattedDate(defaultValues.date) : "", // // OR
+  const [inputs, setInputs] = useState({
+    amount: {
+      value: defaultValues ? defaultValues.amount.toString() : "",
+      // isValid: !!defaultValues,
+      isValid: true,
+    },
+    date: {
+      value: defaultValues ? getFormattedDate(defaultValues.date) : "",
+      // isValid: !!defaultValues,
+      isValid: true,
+    }, // // OR
     // date: defaultValues ? defaultValues.date.toISOString().slice(0, 10) : "",
-    description: defaultValues ? defaultValues.description : "",
+    description: {
+      value: defaultValues ? defaultValues.description : "",
+      // isValid: !!defaultValues, // OR
+      isValid: true,
+      // isValid: defaultValues ? true : false,
+    },
   });
 
   function inputChangedHandler(inputIdentifier, enteredValue) {
-    setInputValues((curInputValues) => {
+    setInputs((curInputs) => {
       return {
-        ...curInputValues,
-        [inputIdentifier]: enteredValue,
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true },
       };
     });
   }
 
   function submitHandler() {
     const expenseData = {
-      amount: +inputValues.amount, // the + converts the value to a number...
-      date: new Date(inputValues.date),
-      description: inputValues.description,
+      amount: +inputs.amount.value, // the + converts the value to a number...
+      date: new Date(inputs.date.value),
+      description: inputs.description.value,
     };
+
+    const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+    const dateIsValid = expenseData.date.toString() !== "Invalid Date";
+    const descriptionIsValid = expenseData.description.trim().length > 0;
+
+    if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+      // Alert.alert("Invalid input", "Please check your input values");
+
+      setInputs((curInputs) => {
+        return {
+          amount: { value: curInputs.amount.value, isValid: amountIsValid },
+          date: { value: curInputs.date.value, isValid: dateIsValid },
+          description: {
+            value: curInputs.description.value,
+            isValid: descriptionIsValid,
+          },
+        };
+      });
+      return;
+    }
 
     onSubmit(expenseData);
   }
+
+  const formIsValid =
+    !inputs.amount.isValid ||
+    !inputs.date.isValid ||
+    !inputs.description.isValid;
 
   return (
     <View style={styles.form}>
@@ -38,33 +77,41 @@ function ExpenseForm({ onCancel, submitButtonLabel, onSubmit, defaultValues }) {
         <Input
           style={styles.rowInput}
           label="Amount"
+          invalid={!inputs.amount.isValid}
           textInputConfig={{
             keyboardType: "decimal-pad",
             onChangeText: inputChangedHandler.bind(this, "amount"),
-            value: inputValues.amount,
+            value: inputs.amount.value,
           }}
         />
         <Input
           label="Date"
+          invalid={!inputs.date.isValid}
           style={styles.rowInput}
           textInputConfig={{
             placeholder: "YYYY-MM-DD",
             maxLength: 10,
             onChangeText: inputChangedHandler.bind(this, "date"),
-            value: inputValues.date,
+            value: inputs.date.value,
           }}
         />
       </View>
       <Input
         label="Description"
+        invalid={!inputs.description.isValid}
         textInputConfig={{
           multiline: true,
           //   autoCapitalize: "sentences"
           // autoCorrent: false // default is true
           onChangeText: inputChangedHandler.bind(this, "description"),
-          value: inputValues.description,
+          value: inputs.description.value,
         }}
       />
+      {formIsValid && (
+        <Text style={styles.errorText}>
+          Invalid Input values - please check your entered data!
+        </Text>
+      )}
       <View style={styles.buttonsContainer}>
         <Button mode="flat" onPress={onCancel} style={styles.button}>
           Cancel
@@ -97,6 +144,11 @@ const styles = StyleSheet.create({
   },
   rowInput: {
     flex: 1,
+  },
+  errorText: {
+    textAlign: "center",
+    color: GlobalStyles.colors.error500,
+    margin: 8,
   },
   buttonsContainer: {
     flexDirection: "row",
--- 09-HandlingUserInput-ExpenseTrackingApp/util/date.js
+++ 09-HandlingUserInput-ExpenseTrackingApp/util/date.js
@@ -1,7 +1,7 @@
 export function getFormattedDate(date) {
-  return date.toISOString().slice(0, 10);
+  // return date.toISOString().slice(0, 10);
   // // OR
-  // return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
 export function getDateMinusDays(date, days) {

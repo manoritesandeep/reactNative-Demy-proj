bd3c5751de02d05e57984424fd49ee3d5bf04436
manoritesandeep <manoritesandeep@gmail.com>
2025-04-29 06:15:07 -0700

    backend setup with firebase... installed axios for HTTP requests... sending POST... fetching data GET request... Transforming and using fetched data...

--- 10-SendingHTTPReq-ExpenseTrackingApp/screens/ManageExpense.js
+++ 10-SendingHTTPReq-ExpenseTrackingApp/screens/ManageExpense.js
@@ -6,6 +6,7 @@ import { GlobalStyles } from "../constants/Styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { storeExpense } from "../util/http";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -36,6 +37,7 @@ function ManageExpense({ route, navigation }) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
+      storeExpense(expenseData);
       expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();

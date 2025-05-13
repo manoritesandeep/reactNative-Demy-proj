9561a9631d62ce58289407d32b2ba1ac15393b19
manoritesandeep <manoritesandeep@gmail.com>
2025-05-12 22:24:41 -0700

    Image picker final...

--- 12-Native-Features/components/Places/ImagePicker.js
+++ 12-Native-Features/components/Places/ImagePicker.js
@@ -7,6 +7,7 @@ import {
 import { useState } from "react";
 
 import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
   const [pickedImage, setPickedImage] = useState();
@@ -59,7 +60,10 @@ function ImagePicker() {
   return (
     <View>
       <View style={styles.imagePreview}>{imagePreview}</View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
+      {/* <Button title="Take Image" onPress={takeImageHandler} /> */}
     </View>
   );
 }
@@ -81,86 +85,3 @@ const styles = StyleSheet.create({
     height: "100%",
   },
 });
-
-// // below code does not render image file... small bug
-// import { useState } from "react";
-// import { Alert, Button, Image, View, Text, StyleSheet } from "react-native";
-// import {
-//   launchCameraAsync,
-//   PermissionStatus,
-//   useCameraPermissions,
-// } from "expo-image-picker";
-// import { Colors } from "../../constants/colors";
-
-// function ImagePicker() {
-//   const [pickedImage, setPickedImage] = useState();
-
-//   const [cameraPermissionInformation, requestPermission] =
-//     useCameraPermissions();
-
-//   async function verifyPermissions() {
-//     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
-//       const permissionResponse = await requestPermission();
-
-//       return permissionResponse.granted;
-//     }
-//     if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
-//       Alert.alert(
-//         "Insufficient Permissions!",
-//         "You need to grant camera permissions to use this app."
-//       );
-//       return false;
-//     }
-//     // If we make it into neither of these if-checks,
-//     // we know that we're not in undetermined state and it was not denied,
-//     // so we do have the permission to use the camera and hence I'll return true here.
-//     return true;
-//   }
-
-//   async function takeImageHandler() {
-//     const hasPermission = await verifyPermissions();
-
-//     if (!hasPermission) {
-//       return;
-//     }
-
-//     const image = await launchCameraAsync({
-//       allowsEditing: true,
-//       aspect: [16, 9],
-//       quality: 0.5,
-//     });
-//     // console.log(image);
-//     setPickedImage(image.uri);
-//   }
-//   // if we have no image then display message
-//   let imagePreview = <Text>No image taken yet.</Text>;
-
-//   if (pickedImage) {
-//     imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
-//   }
-
-//   return (
-//     <View>
-//       <View style={styles.imagePreview}>{imagePreview}</View>
-//       <Button title="Take Image" onPress={takeImageHandler} />
-//     </View>
-//   );
-// }
-
-// export default ImagePicker;
-
-// const styles = StyleSheet.create({
-//   imagePreview: {
-//     width: "100%",
-//     height: 200,
-//     marginVertical: 8,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: Colors.primary100,
-//     borderRadius: 4,
-//   },
-//   image: {
-//     width: "100%",
-//     height: "100%",
-//   },
-// });

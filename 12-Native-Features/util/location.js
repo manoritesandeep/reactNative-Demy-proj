74138b42ce34e9ee0f3dd4c09792884b1a223e78
manoritesandeep <manoritesandeep@gmail.com>
2025-05-13 18:03:47 -0700

    removed console log for GAPIKEY...

--- 12-Native-Features/util/location.js
+++ 12-Native-Features/util/location.js
@@ -3,6 +3,5 @@ import { GOOGLE_API_KEY } from "@env";
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=500x300&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
 
-  console.log("GOOGLE API KEY: ", GOOGLE_API_KEY);
   return imagePreviewUrl;
 }

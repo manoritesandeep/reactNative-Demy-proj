8a86846945576aa138e3fee5cbfd7f7b175d8a3a
manoritesandeep <manoritesandeep@gmail.com>
2025-05-13 20:34:52 -0700

    updated picked location...Map.j

--- 12-Native-Features/screens/Map.js
+++ 12-Native-Features/screens/Map.js
@@ -1,14 +1,86 @@
-import { StyleSheet } from "react-native";
+import { useCallback, useLayoutEffect, useState } from "react";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import IconButton from "../components/UI/IconButton";
 
-function Map() {
+function Map({ navigation }) {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView initialRegion={region} style={styles.map}></MapView>;
+
+  function selectLocationHandler(event) {
+    // console.log("Event Map.js: ", event);
+
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "No location picked.",
+        "You have to pick a location (by tapping on the map) first!"
+      );
+      return;
+    }
+    navigation.navigate("AddPlace", {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  // // we should set our header options to add this header button from inside this map screen component.
+  // // And you did learn how this can be done with help of use layout effect, which is imported from React.
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
+
+  {
+    /* 
+  Very impt note:
+  And the dependencies are the navigation prop here, which I'm using in this effect function
+  and also the savePickedLocationHandler function.
+  And since I'm passing a function here to a wide multiple re-render cycles or potentially even infinite loops,
+  I will wrap this savePickedLocationHandler function with React's use callback hook.  
+  use callback which is a hook imported from React, which helps us ensure that a function defined inside
+  of a component is not recreated unnecessarily.
+  */
+  }
+
+  return (
+    <MapView
+      initialRegion={region}
+      style={styles.map}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
